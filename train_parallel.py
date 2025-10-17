7c929fe14c057004cab8c34cdb57305d0aac4b73
shplok <sawyerbowerman2@gmail.com>
2025-10-17 10:51:28 -0400

    wrap in if __name__ == '__main__': for multiprocessing support

--- train_parallel.py
+++ train_parallel.py
@@ -10,54 +10,58 @@ def make_env(window_index):
         return HasteEnv(mouse_sensitivity=500, window_index=window_index)
     return _init
 
-num_envs = 2
-print(f"setting up {num_envs} parallel game instances")
+if __name__ == '__main__':
+    print("creating parallel environments")
 
-env = SubprocVecEnv([make_env(i) for i in range(num_envs)])
+    # Start with 2 environments
+    num_envs = 2
+    print(f"setting up {num_envs} parallel game instances")
 
-print("creating ppo agent")
-model = PPO(
-    "MlpPolicy",
-    env,
-    verbose=1,
-    learning_rate=3e-4,
-    n_steps=2048,
-    batch_size=64,
-    n_epochs=10,
-    gamma=0.99,
-    tensorboard_log="./logs/",
-    device='cpu'
-)
+    env = SubprocVecEnv([make_env(i) for i in range(num_envs)])
 
-checkpoint_callback = CheckpointCallback(
-    save_freq=10000,
-    save_path="./models/",
-    name_prefix="haste_ppo_parallel",
-    save_replay_buffer=False,
-    save_vecnormalize=False,
-)
-
-print(f"\nstarting training in 10 seconds")
-print(f"make sure {num_envs} haste windows are open side-by-side")
-print("window 0: left side, window 1: right side")
-time.sleep(10)
-
-print("training started")
-print(f"{num_envs}x parallel training enabled")
-
-try:
-    model.learn(
-        total_timesteps=1000000,
-        callback=checkpoint_callback,
-        progress_bar=True
+    print("creating ppo agent")
+    model = PPO(
+        "MlpPolicy",
+        env,
+        verbose=1,
+        learning_rate=3e-4,
+        n_steps=2048,
+        batch_size=64,
+        n_epochs=10,
+        gamma=0.99,
+        tensorboard_log="./logs/",
+        device='cpu'
     )
-    
-    model.save("models/haste_ppo_parallel_final")
 
-except KeyboardInterrupt:
-    print("training interrupted")
-    model.save("models/haste_ppo_parallel_interrupted")
-finally:
-    env.close()
+    checkpoint_callback = CheckpointCallback(
+        save_freq=10000,
+        save_path="./models/",
+        name_prefix="haste_ppo_parallel",
+        save_replay_buffer=False,
+        save_vecnormalize=False,
+    )
 
-print("training complete")
+    print(f"\nstarting training in 10 seconds")
+    print(f"make sure {num_envs} haste windows are open side-by-side")
+    print("window 0: left side, window 1: right side")
+    time.sleep(10)
+
+    print("training started")
+    print(f"{num_envs}x parallel training enabled")
+
+    try:
+        model.learn(
+            total_timesteps=1000000,
+            callback=checkpoint_callback,
+            progress_bar=True
+        )
+        
+        model.save("models/haste_ppo_parallel_final")
+
+    except KeyboardInterrupt:
+        print("training interrupted")
+        model.save("models/haste_ppo_parallel_interrupted")
+    finally:
+        env.close()
+
+    print("training complete")
